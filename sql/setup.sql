ff984d6f3a5169b8040ae199b791ffdbca4b28fc
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-23 16:36:34 -0700

    added git_users to sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,8 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS git_users;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    email VARCHAR,
+    avatar VARCHAR
+);
