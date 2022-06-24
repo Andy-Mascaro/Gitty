89a1f6fc7660cd35276a759bcc5efe849de73a86
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-23 18:00:37 -0700

    added redirect user to api dashboard

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,23 @@ describe('auth routes', () => {
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
   });
+
+  it('should login and redirect users to api/v1/github/dashboard', async () => {
+    const resp = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      username: 'Bob',
+      email: 'bob@bob.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
